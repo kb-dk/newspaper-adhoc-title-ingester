85ec5eca33cccacafca6ac37d75d54fe22d7f39c
Kåre Fiedler Christiansen <kfc@statsbiblioteket.dk>
2015-07-05 10:24:09 +0200

    Use newest framework and newest sbutil

--- src/main/java/dk/statsbiblioteket/newspaper/adhoctitleingester/AdhocTitleIngester.java
+++ src/main/java/dk/statsbiblioteket/newspaper/adhoctitleingester/AdhocTitleIngester.java
@@ -7,7 +7,7 @@ import dk.statsbiblioteket.doms.central.connectors.EnhancedFedoraImpl;
 import dk.statsbiblioteket.doms.central.connectors.fedora.ChecksumType;
 import dk.statsbiblioteket.doms.central.connectors.fedora.pidGenerator.PIDGeneratorException;
 import dk.statsbiblioteket.doms.central.connectors.fedora.templates.ObjectIsWrongTypeException;
-import dk.statsbiblioteket.doms.webservices.authentication.Credentials;
+import dk.statsbiblioteket.sbutil.webservices.authentication.Credentials;
 
 import javax.xml.bind.JAXBException;
 import java.io.File;
